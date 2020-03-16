4dc87a65895807e0767ecafecbf635cf9c75a26c
Mayur1009 <mayur.k.shende@gmail.com>
2020-03-16 23:03:33 +0530

    Initial Commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crop Identification App'),
+      ),
+    );
+  }
+}
