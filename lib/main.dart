dc5867c809837cf432e22d9d9c3704cf8c42739f
Mayur1009 <mayur.k.shende@gmail.com>
2020-03-18 16:21:58 +0530

    Changing theme is working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 import './screens/home_page.dart';
 import './providers/image_object.dart';
@@ -10,18 +11,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ImageObject(),
-      child: MaterialApp(
-        title: 'Crop Identification',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+    return new DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => new ThemeData(
+          primarySwatch: Colors.indigo,
+          brightness: brightness,
         ),
-        home: MyHomePage(),
-      ),
+        themedWidgetBuilder: (context, theme) {
+          return ChangeNotifierProvider(
+            create: (ctx) => ImageObject(),
+            child: new MaterialApp(
+              title: 'Flutter Demo',
+              theme: theme,
+              home: new MyHomePage(),
+            ),
+          );
+        }
     );
+
   }
 }
-
-// TODO: Make the ImageWidget tapable and open a image viewer on tapped.
-// TODO: **Not so imp** Try to implement gallery as a tab in homepage.
